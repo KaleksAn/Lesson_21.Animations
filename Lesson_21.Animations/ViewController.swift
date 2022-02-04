dce474e0802002b2134a914ab7667928cf01d5a9
Alex Kan <alx.kan037@gmail.com>
2022-02-04 09:42:58 +0500

    Initial Commit

---
+++ Lesson_21.Animations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_21.Animations
+//
+//  Created by Aleksandr Kan on 04.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
